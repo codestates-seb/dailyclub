f73c77ffed8efa5d07b10bdf4bdc5a6522568925
cjsgmail <cjs0097@gmail.com>
2022-09-18 17:49:38 +0900

    [FE] fix: Layout height

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,29 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const FooterBox = styled.div`
-    height: 150px;
-    width: 100%;
-    background-color: #262729;
-`
+  height: 150px;
+  width: 100%;
+  background-color: #262729;
+`;
 const FooterText1 = styled.h1`
-    padding-top: 30px;
-    color: white;
-    text-align: center;
-`
+  padding-top: 30px;
+  color: white;
+  text-align: center;
+`;
 const FooterText2 = styled.h2`
-    color: white;
-    padding-top: 10px;
-    text-align: center;
-`
+  color: white;
+  padding-top: 10px;
+  text-align: center;
+`;
 export default function Footer() {
-    return (
-        <FooterBox>
-            <FooterText1>
-                Copyright ⓒ Daily Club
-            </FooterText1>
-            <FooterText2>
-                김준형 고민정 문태경 최정석 황인준
-            </FooterText2>
-        </FooterBox>
-    );
-  }
+  return (
+    <FooterBox>
+      <FooterText1>Copyright ⓒ Daily Club</FooterText1>
+      <FooterText2>김준형 고민정 문태경 최정석 황인준</FooterText2>
+    </FooterBox>
+  );
+}
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -6,6 +6,7 @@ const Content = styled.main`
   position: relative;
   top: 60px;
   width: 60%;
+  height: 100vh;
   /* position때문에 justify, align center 적용 안됨*/
   left: 50%;
   transform: translate(-50%, 0);
