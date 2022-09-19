2e9bb524b2578a4147d929e909e45d2bf062749d
Nelljun <34996983+nelljun@users.noreply.github.com>
2022-09-19 15:16:53 +0900

    [BE] docs: document bookmark api (#32)
     [BE] docs: document bookmark api

--- server/src/main/java/com/codestates/team5/dailyclub/bookmark/service/BookmarkService.java
+++ server/src/main/java/com/codestates/team5/dailyclub/bookmark/service/BookmarkService.java
@@ -1,4 +1,22 @@
 package com.codestates.team5.dailyclub.bookmark.service;
 
+import com.codestates.team5.dailyclub.bookmark.entity.Bookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookmarkService {
+    public Bookmark createBookmark(Long loginUserId, Long questionId) {
+        return null;
+    }
+
+    public Page<Bookmark> findBookmarks(int page, int size, Long loginUserId) {
+        return null;
+    }
+
+    public void deleteBookmark(Long id) {
+
+    }
 }
