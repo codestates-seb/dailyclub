19a1b864fd086e61753177059482580f14aacde4
nelljun <nelljun.kim@gmail.com>
2022-09-19 12:09:44 +0900

    [BE] docs: Notification API Documentation

--- server/src/main/java/com/codestates/team5/dailyclub/notification/entity/Notification.java
+++ server/src/main/java/com/codestates/team5/dailyclub/notification/entity/Notification.java
@@ -1,6 +1,7 @@
 package com.codestates.team5.dailyclub.notification.entity;
 
 
+import com.codestates.team5.dailyclub.common.audit.Auditable;
 import com.codestates.team5.dailyclub.program.entity.Program;
 import com.codestates.team5.dailyclub.user.entity.User;
 import lombok.AllArgsConstructor;
@@ -16,12 +17,11 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Notification {
+public class Notification extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
@@ -31,12 +31,46 @@ public class Notification {
     @JoinColumn(name = "program_id")
     private Program program;
 
+    @Enumerated(EnumType.STRING)
     @NotNull
-    private String text;
+    private NotificationType notificationType;
 
+    @Enumerated(EnumType.STRING)
     @NotNull
-    private Character statusRead = 'F';
+    private StatusRead statusRead = StatusRead.UNREAD;
 
+    //알림 읽음 표시 메서드
+    public void changeStatusRead() {
+        this.statusRead = StatusRead.READ;
+    }
 
+    //알림 읽었는지 여부
+    @Getter
+    public enum StatusRead {
+        READ("READ"), UNREAD("UNREAD");
+
+        private String status;
+
+        StatusRead(String status) {
+            this.status = status;
+        }
+    }
+
+    /**
+     * 알림 종류
+     * - UPDATE : 신청한 프로그램 변경 사항 있을 때
+     * - DDAY : 신청한 프로그램 시작 당일일 때
+     * - COMPLETE : 프로그램 신청 완료했을 때
+     */
+    @Getter
+    public enum NotificationType {
+        UPDATE("UPDATE"), DDAY("DDAY"), APPLY_COMPLETE("APPLY_COMPLETE");
+
+        private String type;
+
+        NotificationType(String type) {
+            this.type = type;
+        }
+    }
 }
 
