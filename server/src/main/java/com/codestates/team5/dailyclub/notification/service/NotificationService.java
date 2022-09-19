d030494c46fff70365bff6bddd92479f405f7694
Nelljun <34996983+nelljun@users.noreply.github.com>
2022-09-19 13:36:58 +0900

    [BE] docs: Notification API Documentation (#29)
     [BE] docs: Notification API Documentation

--- server/src/main/java/com/codestates/team5/dailyclub/notification/service/NotificationService.java
+++ server/src/main/java/com/codestates/team5/dailyclub/notification/service/NotificationService.java
@@ -1,4 +1,34 @@
 package com.codestates.team5.dailyclub.notification.service;
 
+import com.codestates.team5.dailyclub.notification.entity.Notification;
+import com.codestates.team5.dailyclub.notification.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public Page<Notification> findNotifications(int page, int size, Long loginUserId) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by("id").descending());
+        return notificationRepository.findByUserId(pageRequest, loginUserId);
+    }
+
+    public void deleteNotification(Long id) {
+        notificationRepository.deleteById(id);
+    }
+
+    public void updateStatusRead(Long id) {
+        notificationRepository.findById(id)
+                .ifPresent(Notification::changeStatusRead);
+    }
 }
