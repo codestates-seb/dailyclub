840f40c9ca9dc85859cf3f59c208c533c1b86724
nelljun <nelljun.kim@gmail.com>
2022-09-21 15:49:19 +0900

    [BE] feat: create program with program image

--- server/src/main/java/com/codestates/team5/dailyclub/program/controller/ProgramController.java
+++ server/src/main/java/com/codestates/team5/dailyclub/program/controller/ProgramController.java
@@ -1,6 +1,8 @@
 package com.codestates.team5.dailyclub.program.controller;
 
 import com.codestates.team5.dailyclub.common.dto.MultiResponseDto;
+import com.codestates.team5.dailyclub.image.entity.ProgramImage;
+import com.codestates.team5.dailyclub.image.util.ImageUtils;
 import com.codestates.team5.dailyclub.program.dto.ProgramDto;
 import com.codestates.team5.dailyclub.program.dto.SearchFilterDto;
 import com.codestates.team5.dailyclub.program.entity.Program;
@@ -16,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,11 +26,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Tag(name = "프로그램 API")
@@ -36,6 +41,10 @@ import java.util.List;
 @RequestMapping("/api/programs")
 @RequiredArgsConstructor
 public class ProgramController {
+
+    /**
+     * 프로그램 신청 / 취소 API 만들기!!!
+     */
 
     private final ProgramService programService;
     private final ProgramMapper programMapper;
@@ -47,11 +56,21 @@ public class ProgramController {
             description = "CREATED"
         )
     )
-    @PostMapping
-    public ResponseEntity<ProgramDto.Response> postProgram(@RequestBody ProgramDto.Post programPostDto) {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProgramDto.Response> postProgram(@ParameterObject @ModelAttribute ProgramDto.Post programPostDto,
+                                                           @RequestPart(required = false) MultipartFile imageFile) throws IOException {
         Long loginUserId = 1L;
-        Program program = new Program();
-        return new ResponseEntity<>(programMapper.programToProgramResponseDto(program), HttpStatus.CREATED);
+        Program program = programMapper.programPostDtoToProgram(programPostDto);
+
+        //MultipartFile -> ProgramImage 변환
+        ProgramImage programImage = ImageUtils.parseToProgramImage(imageFile);
+
+        //ProgramService 호출 (Program 등록 후 ProgramImage 등록)
+        Program createdProgram = programService.createProgram(loginUserId, program, programImage);
+
+        ProgramDto.Response response = programMapper.programToProgramResponseDto(createdProgram);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @Operation(summary = "프로그램 수정")
@@ -61,8 +80,9 @@ public class ProgramController {
             description = "OK"
         )
     )
-    @PatchMapping("/{programId}")
-    public ResponseEntity<ProgramDto.Response> patchProgram(@RequestBody ProgramDto.Patch programPatchDto,
+    @PatchMapping(value = "/{programId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProgramDto.Response> patchProgram(@ParameterObject @ModelAttribute ProgramDto.Patch programPatchDto,
+                                                            @RequestPart(required = false) MultipartFile imageFile,
                                                             @PathVariable("programId") Long id) {
         Program program = new Program();
         return new ResponseEntity<>(programMapper.programToProgramResponseDto(program), HttpStatus.OK);
@@ -88,7 +108,7 @@ public class ProgramController {
             description = "OK"
         )
     )
-    @GetMapping
+    @GetMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<MultiResponseDto<ProgramDto.Response>> getPrograms(@Parameter(description = "페이지 번호") @RequestParam int page,
                                                                              @Parameter(description = "한 페이지당 프로그램 수") @RequestParam int size,
                                                                              @ParameterObject @ModelAttribute SearchFilterDto searchFilterDto) {
