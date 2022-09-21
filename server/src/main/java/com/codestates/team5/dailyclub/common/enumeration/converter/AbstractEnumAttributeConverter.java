ec38d5a9f7ac0a66cfc3c29dbb9dd7c3d4308648
nelljun <nelljun.kim@gmail.com>
2022-09-21 15:30:22 +0900

    [BE] feat: Add Converter between Enum and DBdata

---
+++ server/src/main/java/com/codestates/team5/dailyclub/common/enumeration/converter/AbstractEnumAttributeConverter.java
@@ -0,0 +1,25 @@
+package com.codestates.team5.dailyclub.common.enumeration.converter;
+
+import com.codestates.team5.dailyclub.common.enumeration.CommonEnum;
+import com.codestates.team5.dailyclub.common.util.EnumValueConvertUtils;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.AttributeConverter;
+
+@Slf4j
+@Getter
+public class AbstractEnumAttributeConverter<E extends Enum<E> & CommonEnum> implements AttributeConverter<E, String> {
+
+    private Class<E> targetEnumClass;
+
+    @Override
+    public String convertToDatabaseColumn(E attribute) {
+        return EnumValueConvertUtils.toDescription(attribute);
+    }
+
+    @Override
+    public E convertToEntityAttribute(String dbData) {
+        return EnumValueConvertUtils.ofDescription(targetEnumClass, dbData);
+    }
+}
