2c4a9b1277133b2ced926b674a7ea9054e4b5297
cjsgmail <cjs0097@gmail.com>
2022-09-22 13:53:13 +0900

    [FE] feat: Program Create API & fix: URL

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -210,7 +210,7 @@ export default function Header({ isLoggedIn }: { isLoggedIn: boolean }) {
             {isLoggedIn ? (
               <>
                 <Icon>
-                  <Link to="/program/create">
+                  <Link to="/programs/create">
                     <img
                       src={Pen}
                       alt="logo"
