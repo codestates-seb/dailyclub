510a5d9afa33d6fce2d25ab32c4219b0bf9d4c34
nelljun <nelljun.kim@gmail.com>
2022-09-25 09:28:28 +0900

    [BE] chore: delete and modified unused files

--- server/src/main/java/com/codestates/team5/dailyclub/location/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.team5.dailyclub.location.repository;
-
-import com.codestates.team5.dailyclub.location.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository <Location, Long> {
-}
