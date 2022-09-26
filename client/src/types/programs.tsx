8e3835cae65dd7f404fc95cef2ea7419a930bf0d
cjsgmail <cjs0097@gmail.com>
2022-09-26 17:47:56 +0900

    [fe] feat: read program (exclude image & member)

--- client/src/types/programs.tsx
+++ client/src/types/programs.tsx
@@ -1,1 +1,22 @@
-1;
+export interface ProgramDetailVal {
+  bookmarkId: number | null;
+  createdDate: string;
+  id: number;
+  location: string;
+  minKind: number;
+  numOfRecruits: number;
+  programDate: string;
+  programImages: undefined | Blob;
+  programStatus: string;
+  text: string;
+  title: string;
+  writer: {
+    id: number;
+    introduction: null | number;
+    kind: number;
+    loginId: string;
+    nickname: string;
+    picture: null | Blob;
+    role: string;
+  };
+}
